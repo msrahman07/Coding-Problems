dc55ad801ce6919ca0552f82efe56bec3bd2d198
Shahriar <msrahman@sfu.ca>
2022-08-27 23:29:17 -0700

    modified gameDamageCalculator and implemented WorkerMangementSystem

---
+++ C# and ASP.NET CORE/BeehiveManagementSystem/BeehiveManagementSystem/Bee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeehiveManagementSystem
+{
+    internal class Bee
+    {
+        public virtual float CostPerShift { get; }
+        public string Job { get; private set; }
+        public Bee(string job)
+        {
+            Job = job;
+        }
+        public void WorkTheNextShift()
+        {
+            if (HoneyVault.ConsumeHoney(CostPerShift))
+            {
+                DoJob();
+            }
+        }
+        protected virtual void DoJob() { /* the subclass overrides this */ }
+    }
+}
---
+++ C# and ASP.NET CORE/GameDamageCalculator/GameDamageCalculator/WeaponDamage.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDamageCalculator
+{
+    internal class WeaponDamage
+    {
+        /// <summary>
+        /// Contains the calculated damage.
+        /// </summary>
+        public int Damage { get; protected set; }
+        protected int roll;
+        /// <summary>
+        /// Sets or gets the 3d6 roll.
+        /// </summary>
+        public int Roll
+        {
+            get { return roll; }
+            set
+            {
+                roll = value;
+                CalculateDamage();
+            }
+        }
+        protected bool magic;
+        /// <summary>
+        /// True if the sword is magic, false otherwise.
+        /// </summary>
+        public bool Magic
+        {
+            get { return magic; }
+            set
+            {
+                magic = value;
+                CalculateDamage();
+            }
+        }
+        protected bool flaming;
+        /// <summary>
+        /// True if the sword is flaming, false otherwise.
+        /// </summary>
+        public bool Flaming
+        {
+            get { return flaming; }
+            set
+            {
+                flaming = value;
+                CalculateDamage();
+            }
+        }
+        /// <summary>
+        /// Calculates the damage based on the current properties.
+        /// </summary>
+        protected virtual void CalculateDamage()
+        {
+            return;
+        }
+        /// <summary>
+        /// The constructor calculates damage based on default Magic
+        /// and Flaming values and a starting 3d6 roll.
+        /// </summary>
+        /// <param name="startingRoll">Starting 3d6 roll</param>
+        public WeaponDamage(int startingRoll)
+        {
+            roll = startingRoll;
+            CalculateDamage();
+        }
+    }
+}
