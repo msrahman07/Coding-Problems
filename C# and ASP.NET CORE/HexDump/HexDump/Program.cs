357c6595366eebcaac63265082d17fe6c8c11c06
Shahriar <msrahman@sfu.ca>
2022-09-04 21:48:13 -0700

    stream and hexdump

---
+++ C# and ASP.NET CORE/HexDump/HexDump/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var position = 0;
+        using (var reader = new StreamReader("textdata.txt"))
+        {
+            while (!reader.EndOfStream)
+            {
+                var buffer = new char[16];
+                var bytesRead = reader.ReadBlock(buffer, 0, 16);
+
+                Console.Write("{0:x4}: ", position);
+                position += bytesRead;
+
+                for(var i=0; i < 16; i++)
+                {
+                    if(i < bytesRead)
+                    {
+                        Console.Write("{0:x2}: ", (byte)buffer[i]);
+                    }
+                    else
+                    {
+                        Console.Write("   ");
+                    }
+                    if (i == 7)
+                    {
+                        Console.Write("-- ");
+                    }
+                }
+                var bufferContents = new string(buffer);
+                Console.WriteLine(" {0}", bufferContents.Substring(0, bytesRead));
+            }
+        }
+    }
+}
---
+++ C# and ASP.NET CORE/StreamExperiments/StreamExperiments/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var folder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+        var reader = new StreamReader($"{folder}{Path.DirectorySeparatorChar}secret_plan.txt");
+        var writer = new StreamWriter($"{folder}{Path.DirectorySeparatorChar}emailToCaptain.txt");
+        writer.WriteLine("To: CaptainAmazing@objectville.net");
+        writer.WriteLine("From: Commissioner@objectville.net");
+        writer.WriteLine("Subject: Can you save the day... again?");
+        writer.WriteLine();
+        writer.WriteLine("We’ve discovered the Swindler’s terrible plan:");
+
+        while (!reader.EndOfStream)
+        {
+            var lineFromPlan = reader.ReadLine();
+            writer.WriteLine($"The Plan -> {lineFromPlan}");
+        }
+        writer.WriteLine();
+        writer.WriteLine("Can you help us?");
+        writer.Close();
+        reader.Close();
+        /*StreamWriter sw = new StreamWriter("secret_plan.txt");
+        sw.WriteLine("How I'll defeat Captain Amazing");
+        sw.WriteLine("Another genius secret plan by The Swindler");
+        sw.WriteLine("I'll unleash my army of clones upon the citizens of Objectville.");
+        string location = "the mall";
+        for (int number = 1; number <= 5; number++)
+        {
+            sw.WriteLine("Clone #{0} attacks {1}", number, location);
+            location = (location == "the mall") ? "downtown" : "the mall";
+        }
+        sw.Close();*/
+    }
+}
