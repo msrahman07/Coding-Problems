6a95cbd58710cd5f45bf715dfa05e8a8603e3c4d
Shahriar <msrahman@sfu.ca>
2022-12-28 21:57:42 -0800

    K Closest Points to Origin

---
+++ NeetCode/Heap/KClosestPointstoOrigin.py
@@ -0,0 +1,22 @@
+"""
+LeetCode: 973. K Closest Points to Origin
+Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane and an integer k, return the k closest points to the origin (0, 0).
+
+The distance between two points on the X-Y plane is the Euclidean distance (i.e., √(x1 - x2)2 + (y1 - y2)2).
+
+You may return the answer in any order. The answer is guaranteed to be unique (except for the order that it is in).
+"""
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        pts = []
+        for x, y in points:
+            dist = (abs(x - 0) ** 2) + (abs(y - 0) ** 2)
+            pts.append([dist, x, y])
+
+        res = []
+        heapq.heapify(pts)
+        for i in range(k):
+            dist, x, y = heapq.heappop(pts)
+            res.append([x, y])
+        return res
