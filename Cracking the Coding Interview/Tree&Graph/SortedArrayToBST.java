01c9f359af3f5cc4be9017c6849885e42daecd49
Shahriar <msrahman@sfu.ca>
2023-03-08 15:14:05 -0800

    trees&graph - sorted array to bst

---
+++ Cracking the Coding Interview/Tree&Graph/SortedArrayToBST.java
@@ -0,0 +1,32 @@
+public class SortedArrayToBST {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if(nums.length <= 1) {
+            return new TreeNode(nums[0]);
+        }
+        return sortedArrayToBSTHelper(nums, 0, nums.length-1);
+    }
+    private TreeNode sortedArrayToBSTHelper(int[] nums, int left, int right) {
+        if(right < left) {
+            return null;
+        }
+        int mid = (left + right)/2;
+        TreeNode root = new TreeNode(nums[mid]);
+        
+        root.left = sortedArrayToBSTHelper(nums, left, mid-1);
+        root.right = sortedArrayToBSTHelper(nums, mid+1, right);
+
+        return root;
+    } 
+}
