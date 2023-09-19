21cee0ae04dd8757c44e5c8ba41057bf9edef501
Shahriar <msrahman@sfu.ca>
2023-09-18 23:09:19 -0700

    utf8 validation - java

--- Java/ProgrammingProblems/src/main/java/programming/problems/Main.java
+++ Java/ProgrammingProblems/src/main/java/programming/problems/Main.java
@@ -4,14 +4,26 @@ import java.lang.reflect.Type;
 
 public class Main {
     public static void main(String[] args) {
-        ReverseWords rvwords =  new ReverseWords();
-        IntegerStringConversion intString = new IntegerStringConversion();
+//        ReverseWords rvwords =  new ReverseWords();
+//        IntegerStringConversion intString = new IntegerStringConversion();
+//
+//        System.out.println(rvwords.reverseWords("   Hello    World   "));
+//        System.out.println(rvwords.reverseWords("   HelloWorld   "));
+//        System.out.println(rvwords.reverseWords("HelloWorld"));
+//        System.out.println(rvwords.reverseWords(""));
+//        System.out.println((""+(Integer.MIN_VALUE)));
+//        System.out.println((""+(Integer.MAX_VALUE)));
+//        printNumberCalc(1111);
+        int intValue = 97;
+        byte utf8Byte = (byte)(intValue & 0xFF);
+        System.out.printf("UTF-8 Byte: %02X\n", utf8Byte);
+    }
 
-        System.out.println(rvwords.reverseWords("   Hello    World   "));
-        System.out.println(rvwords.reverseWords("   HelloWorld   "));
-        System.out.println(rvwords.reverseWords("HelloWorld"));
-        System.out.println(rvwords.reverseWords(""));
-        System.out.println((""+(Integer.MIN_VALUE)));
-        System.out.println((""+(Integer.MAX_VALUE)));
+    public static void printNumberCalc(int number) {
+        do {
+            int remainder = number % 10;
+            number /= 10;
+            System.out.println(remainder);
+        } while(number > 0);
     }
 }
