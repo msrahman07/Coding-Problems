6e77c9614b0b4eefdf6c22143d4711a8f411311a
Shahriar <msrahman@sfu.ca>
2023-10-29 13:02:07 -0700

    multiply, subtract, divide with only add op

--- Java/ProgrammingProblems/src/main/java/programming/problems/MathOperationsWithAddOnly.java
+++ Java/ProgrammingProblems/src/main/java/programming/problems/MathOperationsWithAddOnly.java
@@ -3,24 +3,72 @@ package programming.problems;
 public class MathOperationsWithAddOnly {
     public int multiply(int a, int b) {
         // a=9, b=11
-        int multiplier = 0;
-        int number = 1;
-        int answer = 0;
-        if(a > b) {
-            multiplier = b;
-            number = a;
-        } else {
-            multiplier = a;
-            number = b;
+        if(b > a) {
+            return multiply(b, a); // algo is faster when b < a
         }
-        // multiplier = 9
-        // number = 11
-        for(int i=0; i<multiplier; i++) {
-            answer += number;
+        int sum = 0;
+        for(int i = 1; i <= abs(b); i++) {
+            sum += a;
         }
-        return answer;
+        if(b < 0) {
+            sum = negate(sum);
+        }
+        return sum;
     }
-    public int divide(int a, int b) {
-        return a/b;
+
+    public int divide(int a, int b) throws ArithmeticException {
+        // a=9, b=11
+        if(b == 0) {
+            throw new ArithmeticException("Divided by zero"); // algo is faster when b < a
+        }
+        int absa = abs(a);
+        int absb = abs(b);
+
+        int product = 0;
+        int x = 0;
+        while(product + absb <= absa) {
+            product += absb;
+            x++;
+        }
+
+        if((a < 0 && b < 0) || (a > 0 && b > 0)) {
+            return x;
+        } else {
+            return negate(x);
+        }
+    }
+
+    private int abs(int num) {
+        return num < 0 ? negate(num) : num;
+    }
+    public int subtract(int a, int b){
+        return a + negate(b);
+    }
+    // Time comp for negate O(num)
+//    private int negate(int num){
+//        int newSign = a < 0 ? 1 : -1;
+//        int neg = 0;
+//
+//        while(num != 0) {
+//            num += newSign;
+//            neg += neg;
+//        }
+//        return neg;
+//    }
+    private int negate(int a){
+        int newSign = a < 0 ? 1 : -1;
+        int neg = 0;
+        int delta = newSign;
+
+        while(a != 0) {
+            boolean differentSigns = (a + delta > 0) != (a > 0);
+            if(a + delta != 0 && differentSigns) {
+                delta = newSign;
+            }
+            a += delta;
+            neg += delta;
+            delta += delta; // double the delta
+        }
+        return neg;
     }
 }
--- Java/ProgrammingProblems/src/test/java/MathOperationsWithAddOnlyTests.java
+++ Java/ProgrammingProblems/src/test/java/MathOperationsWithAddOnlyTests.java
@@ -30,4 +30,56 @@ public class MathOperationsWithAddOnlyTests {
         MathOperationsWithAddOnly op = new MathOperationsWithAddOnly();
         Assertions.assertEquals((11*-2), op.multiply(11, -2));
     }
+    @Test
+    @DisplayName("Test multiply Function, One of them Negative")
+    public void subtractTestPos() {
+        MathOperationsWithAddOnly op = new MathOperationsWithAddOnly();
+        Assertions.assertEquals((99-37), op.subtract(99, 37));
+    }
+    @Test
+    @DisplayName("Test multiply Function, One of them Negative")
+    public void subtractTestOneNeg() {
+        MathOperationsWithAddOnly op = new MathOperationsWithAddOnly();
+        Assertions.assertEquals((99-(-37)), op.subtract(99, -37));
+    }
+    @Test
+    @DisplayName("Test multiply Function, One of them Negative")
+    public void subtractTestBothNeg() {
+        MathOperationsWithAddOnly op = new MathOperationsWithAddOnly();
+        Assertions.assertEquals(((-99)-(-37)), op.subtract(-99, -37));
+    }
+    @Test
+    @DisplayName("Test multiply Function, One of them Negative")
+    public void subtractTestZero() {
+        MathOperationsWithAddOnly op = new MathOperationsWithAddOnly();
+        Assertions.assertEquals((-99), op.subtract(-99, 0));
+    }
+    @Test
+    @DisplayName("Test multiply Function, One of them Negative")
+    public void divideTestByZero() {
+        MathOperationsWithAddOnly op = new MathOperationsWithAddOnly();
+        ArithmeticException thrown = Assertions.assertThrows(ArithmeticException.class, () -> {
+            op.divide(99, 0);
+        }, "Divided by zero");
+        Assertions.assertEquals("Divided by zero", thrown.getMessage());
+    }
+    @Test
+    @DisplayName("Test multiply Function, One of them Negative")
+    public void divideTest12By4() {
+        MathOperationsWithAddOnly op = new MathOperationsWithAddOnly();
+        Assertions.assertEquals(12/4, op.divide(12, 4));
+    }
+
+    @Test
+    @DisplayName("Test multiply Function, One of them Negative")
+    public void divideTest34By3() {
+        MathOperationsWithAddOnly op = new MathOperationsWithAddOnly();
+        Assertions.assertEquals(34/3, op.divide(34, 3));
+    }
+    @Test
+    @DisplayName("Test multiply Function, One of them Negative")
+    public void divideTest0By33() {
+        MathOperationsWithAddOnly op = new MathOperationsWithAddOnly();
+        Assertions.assertEquals(0/33, op.divide(0, 33));
+    }
 }
